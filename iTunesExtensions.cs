d2c78fe201b0faf64922b8c40a1ed1e575b85119
res0uyha <res0uyha@localhost>
2014-08-05 22:54:54 -0500

    new

---
+++ iTunesExtensions.cs
@@ -0,0 +1,33 @@
+﻿using iTunesLib;
+using System.Threading;
+
+namespace MusicBeePlugin
+{
+	static class iTunesExtensions
+	{
+		public static IITPlaylist GetPlaylist(this iTunesApp iTunes, string name)
+		{
+			foreach (IITPlaylist playlist in iTunes.LibrarySource.Playlists)
+			{
+				if (
+					 playlist.Name == name
+					 && playlist.Kind == ITPlaylistKind.ITPlaylistKindUser
+					 && ((IITUserPlaylist)playlist).SpecialKind == ITUserPlaylistSpecialKind.ITUserPlaylistSpecialKindNone
+					 && !((IITUserPlaylist)playlist).Smart
+					 )
+				{
+					return playlist;
+				}
+			}
+			return null;
+		}
+
+		public static void Await(this IITOperationStatus operation)
+		{
+			while (operation.InProgress)
+			{
+				Thread.Sleep(100);
+			}
+		}
+	}
+}
